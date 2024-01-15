065f58a3450707f650eca8339257d159cbf57ef4
Egor Beliaev <egbeliaev@gmail.com>
2024-01-15 14:12:53 +0200

    *

---
+++ models/download.sh
@@ -0,0 +1,6 @@
+wget "https://github.com/facefusion/facefusion-assets/releases/download/models/gfpgan_1.4.onnx"
+wget "https://github.com/facefusion/facefusion-assets/releases/download/models/arcface_w600k_r50.onnx"
+wget "https://github.com/facefusion/facefusion-assets/releases/download/models/arcface_simswap.onnx"
+wget "https://github.com/xinntao/facexlib/releases/download/v0.2.0/parsing_bisenet.pth"
+wget "https://github.com/facefusion/facefusion-assets/releases/download/models/simswap_256.onnx"
+
